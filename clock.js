c159dac085883fe8996013b90a0c0e03bd23732f
Anita Mishra <Anita Mishra>
2022-07-26 14:18:32 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[ data-hour-hand]')
+const minuteHand = document.querySelector('[ data-minute-hand]')
+const secondHand = document.querySelector('[ data-second-hand]')
+
+function setClock(){
+    const currentDate = new Date()
+    const secondRatio = currentDate.getSeconds()/60
+    const minutesratio = (secondRatio + currentDate.getMinutes()) /60
+    const hourRatio = (minutesratio + currentDate.getHours())/12
+    setRotation(secondHand, secondRatio)
+    setRotation(minuteHand, minutesratio)
+    setRotation(hourHand, hourRatio)
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
